dff609a4b2c6502a8b3e154eef0565c71945180b
Bata Zoltán <batazo.dad@gmail.com>
2026-06-30 17:43:19 +0200

    OrderReturnBundle OrderReturnNormalizer

---
+++ src/CoreShop/Bundle/OrderReturnBundle/Serializer/Normalizer/OrderReturnNormalizer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CoreShop\Bundle\OrderReturnBundle\Serializer\Normalizer;
+
+use CoreShop\Component\OrderReturn\Model\OrderReturn;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class OrderReturnNormalizer implements NormalizerInterface
+{
+    /**
+     * @param OrderReturn $object
+     *
+     * @return array
+     */
+    public function normalize($object, ?string $format = null, array $context = []): array
+    {
+        $pdfAttachment = $object->getPdfAttachment();
+        $pdfPath = ($pdfAttachment && $pdfAttachment->getFullPath()) ? \Pimcore\Tool::getHostUrl() . $pdfAttachment->getFullPath() : '';
+
+        return [
+            'returnedAt' => $object->getReturnedAt(),
+            'orderNumber' => $object->getOrderNumber(),
+            'firstname' => $object->getFirstName(),
+            'lastname' => $object->getLastName(),
+            'email' => $object->getEmail(),
+            'pdfPath' => $pdfPath,
+        ];
+    }
+
+    public function supportsNormalization($data, ?string $format = null, array $context = []): bool
+    {
+        return $data instanceof OrderReturn;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            OrderReturn::class => true,
+        ];
+    }
+}
